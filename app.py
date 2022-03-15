4be8ffd7de1c711106e8b7688f5bb41c1aadc1f3
g1pd78 <g1pd78@gmail.com>
2022-03-15 04:07:14 -1000

    post method with required arguments

--- app.py
+++ app.py
@@ -1,18 +1,39 @@
 from flask import Flask
 import sys
 from flask_restful import Api, Resource
+from flask_restful import reqparse
+from flask_restful import inputs
 
 app = Flask(__name__)
 
 api = Api(app)
+parser = reqparse.RequestParser()
 
 
 class HelloWorld(Resource):
     def get(self):
         return {"data":"There are no events for today!"}
 
+    def post(self):
+        args = parser.parse_args()
+        d = {"message": 'The event has been added!', "event": args['event'], "date": str(args['date'].date())}
+        return d
 
-api.add_resource(HelloWorld, '/event/today')
+
+api.add_resource(HelloWorld, '/event')
+
+parser.add_argument(
+    'event',
+    type=str,
+    help="The event name is required!",
+    required=True
+)
+parser.add_argument(
+    'date',
+    type=inputs.date,
+    help="The event date with the correct format is required! The correct format is YYYY-MM-DD!",
+    required=True
+)
 
 
 if __name__ == '__main__':
@@ -20,4 +41,4 @@ if __name__ == '__main__':
         arg_host, arg_port = sys.argv[1].split(':')
         app.run(host=arg_host, port=arg_port)
     else:
-        app.run()
+        app.run(debug=True)
