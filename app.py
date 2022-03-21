c1ddd231793280813ef41a1c1e9cf06f56365b61
g1pd78 <g1pd78@gmail.com>
2022-03-21 09:59:47 -1000

    delete tasks, search by id, print all

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, abort
 import sys
 from flask_restful import Api, Resource
 from flask_restful import reqparse
@@ -8,7 +8,7 @@ import datetime
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///task.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///event.db'
 
 
 resource_fields = {
@@ -19,26 +19,30 @@ resource_fields = {
 
 
 class Task(db.Model):
-    __tablename__ = 'tasks'
-    id: int
-    event: str
-    date: str
+    __tablename__ = 'event'
 
     id = db.Column(db.Integer, primary_key=True)
     event = db.Column(db.String(80), nullable=False)
-    date = db.Column(db.String(80), nullable=False)
+    date = db.Column(db.Date, nullable=False)
 
 
 db.create_all()
 
 api = Api(app)
 parser = reqparse.RequestParser()
+parser2 = reqparse.RequestParser()
 
 
 class Event(Resource):
     @marshal_with(resource_fields)
     def get(self):
-        tasks = Task.query.all()
+        args = parser2.parse_args()
+        tasks = ''
+
+        if not args['start_time'] is None and not args['end_time'] is None:
+            tasks = Task.query.filter(Task.date.between(str(args['start_time'].date()), str(args['end_time'].date()))).all()
+        else:
+            tasks = Task.query.all()
         return tasks
 
     def post(self):
@@ -46,7 +50,7 @@ class Event(Resource):
         task = Task(event=args['event'], date=args['date'].date())
         db.session.add(task)
         db.session.commit()
-        d = {"message": 'The event has been added!', "event": args['event'], "date": str(args['date'].date())}
+        d = {"message": "The event has been added!", "event": args['event'], "date": str(args['date'].date())}
         return d
 
 
@@ -57,8 +61,27 @@ class Today(Resource):
         return tasks
 
 
+class EventById(Resource):
+    @marshal_with(resource_fields)
+    def get(self, event_id):
+        task = Task.query.filter(Task.id == event_id).first()
+        if task is None:
+            abort(404, "The event doesn't exist!")
+        return task
+
+    def delete(self, event_id):
+        task = Task.query.filter(Task.id == event_id).first()
+        if task is None:
+            abort(404, "The event doesn't exist!")
+        else:
+            Task.query.filter(Task.id == event_id).delete()
+            db.session.commit()
+            return {"message": "The event has been deleted!"}
+
+
 api.add_resource(Event, '/event')
 api.add_resource(Today, '/event/today')
+api.add_resource(EventById, '/event/<int:event_id>')
 
 parser.add_argument(
     'event',
@@ -72,7 +95,18 @@ parser.add_argument(
     help="The event date with the correct format is required! The correct format is YYYY-MM-DD!",
     required=True
 )
-
+parser2.add_argument(
+    'start_time',
+    type=inputs.date,
+    help="The event date with the correct format is required! The correct format is YYYY-MM-DD!",
+    required=False
+)
+parser2.add_argument(
+    'end_time',
+    type=inputs.date,
+    help="The event date with the correct format is required! The correct format is YYYY-MM-DD!",
+    required=False
+)
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         arg_host, arg_port = sys.argv[1].split(':')
